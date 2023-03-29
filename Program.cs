21a5718e168795aca09a3fa45e01ef4b1a59248a
Pedro H. Alves <leonkene18@gmail.com>
2023-03-29 10:40:20 -0300

    atividade simples de C#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write($"Digite a primeira nota: ");
+float n1 = float.Parse(Console.ReadLine());
+
+Console.Write($"Digite a segunda nota: ");
+float n2 = float.Parse(Console.ReadLine());
+
+Console.Write($"Digite a terceira nota: ");
+float n3 = float.Parse(Console.ReadLine());
+
+Console.Write($"Digite a quarta nota: ");
+float n4 = float.Parse(Console.ReadLine());
+
+Console.Write($"Digite a quinta nota: ");
+float n5 = float.Parse(Console.ReadLine());
+
+float media = (n1 + n2 + n3 + n4 + n5) / 5;
+Console.WriteLine($"Sua media e {media}");
